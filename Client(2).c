f4c49f466e36c2d557ffba4b31a139b36b3a36da
MaaniacLaughter <54497498+MaaniacLaughter@users.noreply.github.com>
2021-08-16 22:44:31 +0530

    Update Client(2).c

--- Client(2).c
+++ Client(2).c
@@ -1,58 +1,70 @@
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include "inven_func.h"
 #include<string.h>
 
+//int slots_used = -1;
 
 int main()
 {
-	printf("Enter user: \n1.Customer\n2.Employee");
-	int choice;
+	printf("Enter user: \n1.Customer\n2.Employee\n");
+	int choice, ch2;
+    char pname[30];
   	scanf("%d",&choice);
   	if(choice==1)
  	{
-  		printf("What do you want to do? \n1.Search product \n2.Add product to cart \n3.Remove product from cart \n4.View cart\n5.Total bill");
-		int c_ch1;  
-   		scanf("%d", &c_ch1);
+  		printf("What do you want to do? \n1.Search product \n2.Add product to cart \n3.Remove product from cart \n4.View cart\n5.Total bill\n");
+		int ch1;
+   		scanf("%d", &ch1);
     		do{
 		switch(ch1)
 		{
 			case 1:
-				char entered_prod[30];
+
 				printf("\nEnter the name of the product:");
-        			//char entered_prod; ---turn to string
-				scanf("%s", &entered_prod);
-				//if(entered_prod in s.prod_name array) ---syntax ---struct details
-					View_product(entered_prod);
-				else
+        			//char pname; ---turn to string
+				scanf("%s", &pname);
+				//if(pname in s.prod_name array) ---syntax ---struct details
+					View_product(pname);
+				/*else
 				{
 					printf("\nProduct not found/not available");
-				}
+				}*/
 				break;
-			case 2: 
-				add_to_cart();
+
+
+			case 2:
+			    printf("\nEnter the name of the product to be added to cart:");
+			    scanf("%s", &pname);
+				add_to_cart(pname);
 				break;
-			case 3: 
+            /*
+			case 3:
 				remove_from_cart();
 				break;
-			case 4: 
+            */
+			case 4:
 				printf("\nDisplaying cart:");
 				display_cart();
 				break;
+            /*
       		case 5:
       			total_bill();
-       			break;
+       			break; */
 			default:
-				printf("\nInvalid input"); 
+				printf("\nInvalid input");
 		} //switch
  		printf("\nDo you want to quit the program? (Press 0): ");
 		scanf("%d", &ch2);
+		if(ch2!=0)
+        {
+            printf("What do you want to do? \n1.Search product \n2.Add product to cart \n3.Remove product from cart \n4.View cart\n5.Total bill\n");
+            scanf("%d", &ch1);
+        }
 		}while(ch2!=0);     //do while
 	} //if statement
- 
- 	else if(choice==2)
+
+ 	/*else if(choice==2)
  	{
   		printf("Enter employee login password:");
    		int emp_pass = 12345;    //change to string?
@@ -62,7 +74,7 @@ int main()
     			printf("\nInvalid password\n");
     		else
     		{
-   			int ch1, ch2, ref; 
+   			int ch1, ch2, ref;
 			printf("What do you want to do? \n1.View inventory \n2.Add a product record \n3.Delete a record \n4.Edit product details\n");
                   scanf("%d", &ch1);
 			do{
@@ -82,45 +94,45 @@ int main()
                               else
                               	printf("Invalid input");
 					break;
-				case 2: 
+				case 2:
 					add_pdt();
 					break;
-				case 3: 
+				case 3:
 					delete_pdt();
 					break;
-				case 4: 
+				case 4:
 					printf("\nEnter ref no. of the product: ");
 					scanf("%d", &ref);
 					edit_pdt(ref);
 					break;
 				default:
-					printf("\nInvalid input"); 
+					printf("\nInvalid input");
 			}
    		printf("\nDo you want to quit the program? (Press 0): ");
 		scanf("%d", &ch2);
 		}while(ch2);
     	}//else
  	}//else if
-  
+  */
   else
   {
   	printf("Invalid input");
     	exit(0); 											// go back to choice
   }
-  
-	
-  
-  
+
+
+
+
   /*
-  
+
   printf("\nDo you want to quit the program? (Press 0): ");
 	scanf("%d", &ch2);
 	}while(ch2);
- 
+
  */
-  
-  
-  
-  
+
+
+
+
   return 0;
 }
